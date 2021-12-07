c1e3581d6a7efc4f77f89385e3a13a7716e935a5
nthieu29 <ngotrunghieu@live.com>
2021-12-07 16:24:04 +0800

    add javadoc

--- src/main/java/algs/graph/directed/StrongConnectedComponent.java
+++ src/main/java/algs/graph/directed/StrongConnectedComponent.java
@@ -1,5 +1,13 @@
 package algs.graph.directed;
 
+/***
+ * Definition:
+ * - Vertices v and w are strongly connected if there is both a directed path from v to w and a directed path from w to v.
+ * - A strong component is a maximal subset of strongly-connected vertices.
+ * Solution:
+ * 1. Run DFS on the reversed graph to create reverse post order.
+ * 2. Run DFS by considering the vertices in reverse post order (of step 1).
+ */
 public class StrongConnectedComponent {
     private int[] id;
     private boolean[] marked;

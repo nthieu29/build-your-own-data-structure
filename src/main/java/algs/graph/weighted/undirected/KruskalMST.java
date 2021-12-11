5f44c65a7612104ea7cdd768f2d5f22d10982cc5
nthieu29 <ngotrunghieu@live.com>
2021-12-11 10:47:21 +0800

    add javadoc

--- src/main/java/algs/graph/weighted/undirected/KruskalMST.java
+++ src/main/java/algs/graph/weighted/undirected/KruskalMST.java
@@ -6,6 +6,21 @@ import algs.queue.Queue;
 import algs.uf.QuickUnion;
 import algs.uf.UF;
 
+/***
+ * Minimum Spanning Tree (MST) of graph G is the subgraph T that is both:
+ *  - Tree (connected and acyclic)
+ *  - Spanning (includes all the vertices)
+ * Kruskal's algorithm: consider edges in ascending order of weights, add the next edge to tree T unless doing so would
+ * create a cycle.
+ *  - Build min order priority queue of edges in graph.
+ *  - Build Union Find data structure.
+ *  - Poll edges from priority queue, for each edge:
+ *      + If 2 vertices of edge are not connected in Union Find:
+ *          + Add that edge to MST.
+ *          + Connect 2 vertices in Union Find
+ * Time complexity: E log E
+ * Space complexity: E + V
+ */
 public class KruskalMST implements MST {
     private Queue<Edge> minimumSpanningTree;
 
